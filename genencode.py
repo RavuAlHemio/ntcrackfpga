314005586ecdb9cef5fd05961909dd357deeb660
Ondřej Hošek <ondra.hosek@gmail.com>
2022-11-02 03:07:03 +0100

    genencode: reverse bytes, use jinja

--- genencode.py
+++ genencode.py
@@ -1,72 +1,73 @@
 #!/usr/bin/env python3
 import math
 import sys
+from typing import NamedTuple
+import jinja2
+
 
 PASSWORD_CHARS = 20
 TEMPLATE = """
 function [511:0] password_to_md4_data(
-    input [{input_topbit}:0] in,
-    input [{len_topbit}:0] len);
+    input [{{ password_chars * 8 - 1 }}:0] in,
+    input [{{ len_topbit }}:0] len);
     begin
         case (len)
-{md4_cases}
+            {%- for n in range(password_chars+1) %}
+                {{ n }}: password_to_md4_data = {
+                    {#- conversion to UTF-16 #}
+                    {%- for i in range(n)|reverse %}
+                        {%- if not loop.first %}, {% endif -%}
+                        in[{{ 8*i + 7 }}:{{ 8 * i }}], 8'h0
+                    {%- endfor %}
+
+                    {#- padding #}
+                    {%- if n > 0 %}, {% endif -%}
+                    8'h80, {{ n|zero_padding_bit_count }}'h0
+                    {%- for b in n|utf16_bit_count_le_bytes -%}
+                    , 8'h{{ b|hex }}
+                    {%- endfor -%}
+                };
+            {%- endfor %}
             default: password_to_md4_data = 512'h00;
         endcase
     end
 endfunction
 """
 
+
+def zero_padding_bit_count(ascii_byte_count: int) -> int:
+    utf16_byte_count = ascii_byte_count * 2
+    # 64 bytes per chunk minus 8 bytes length minus 1 byte 0x80 minus data bytes
+    zero_padding_byte_count = 64 - (8 + 1 + utf16_byte_count)
+    return zero_padding_byte_count * 8
+
+
+def utf16_bit_count_le_bytes(ascii_byte_count: int) -> bytes:
+    utf16_byte_count = ascii_byte_count * 2
+    utf16_bit_count = utf16_byte_count * 8
+    return utf16_bit_count.to_bytes(8, "little")
+
+
+def hex_filter(i: int) -> str:
+    return f"{i:x}"
+
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: genencode.py OUTPUT", file=sys.stderr)
         sys.exit(1)
 
-    utf16_topbit = PASSWORD_CHARS * 8 * 2 - 1
     input_topbit = PASSWORD_CHARS * 8 - 1
     len_topbit = math.ceil(math.log2(input_topbit + 1))
 
-    md4_cases = []
-    for n in range(PASSWORD_CHARS+1):
-        case_pieces = [f"            {n}: password_to_md4_data = ", "{ "]
-
-        # generate the conversion to UTF-16
-        for i in range(n):
-            input_piece_topbit = input_topbit - i * 8
-            input_piece_bottombit = input_piece_topbit - 7
-            if i != 0:
-                case_pieces.append(", ")
-            case_pieces.append(f"in[{input_piece_topbit}:{input_piece_bottombit}]")
-            case_pieces.append(", 8'h0")
-
-        # generate the padding
-        if n > 0:
-            case_pieces.append(", ")
-        case_pieces.append("8'h80")
-
-        utf16_byte_count = n * 2
-        utf16_bit_count = utf16_byte_count * 8
-        # 64 bytes per chunk minus 8 bytes length minus 1 byte 0x80 minus data bytes
-        zero_padding_byte_count = 64 - (8 + 1 + utf16_byte_count)
-        zero_padding_bit_count = zero_padding_byte_count*8
-        case_pieces.append(f", {zero_padding_bit_count}'h0")
-
-        # generate the length (in bits as 64-bit little-endian)
-        len_bytes = utf16_bit_count.to_bytes(2, "little")
-        for lb in len_bytes:
-            case_pieces.append(f", 8'h{lb:02x}")
-        len_bits_remain = 64 - 8*len(len_bytes)
-        case_pieces.append(f", {len_bits_remain}'h00")
-
-        case_pieces.append(" };")
-        md4_cases.append("".join(case_pieces))
-
-    md4_cases_str = "\n".join(md4_cases)
-
-    output = (
-        TEMPLATE
-            .replace("{input_topbit}", str(input_topbit))
-            .replace("{len_topbit}", str(len_topbit))
-            .replace("{md4_cases}", md4_cases_str)
+    env = jinja2.Environment(undefined=jinja2.StrictUndefined)
+    env.filters["zero_padding_bit_count"] = zero_padding_bit_count
+    env.filters["utf16_bit_count_le_bytes"] = utf16_bit_count_le_bytes
+    env.filters["hex"] = hex_filter
+    tpl = env.from_string(TEMPLATE)
+    output = tpl.render(
+        password_chars=PASSWORD_CHARS,
+        len_topbit=len_topbit,
     )
 
     with open(sys.argv[1], "w", encoding="us-ascii") as f:
