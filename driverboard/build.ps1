82f76cc12a51d430cfb2ef7150c42301bb9ffa41
Ondřej Hošek <ondra.hosek@gmail.com>
2022-11-06 20:45:56 +0100

    driverboard: add build scripts

---
+++ driverboard/build.ps1
@@ -0,0 +1,48 @@
+[CmdletBinding()]
+Param (
+    [switch]
+    $NoBuild,
+
+    [switch]
+    $NoProgram,
+
+    [switch]
+    $Debugger,
+
+    [string]
+    $BinaryName="ntcrackfpga_driver"
+)
+
+
+If (-not $NoBuild)
+{
+    & cargo build --release
+    If ($LASTEXITCODE -ne 0)
+    {
+        Return 1
+    }
+
+    & rust-objcopy --output-target=binary ".\target\thumbv6m-none-eabi\release\$BinaryName" ".\$BinaryName.bin"
+    If ($LASTEXITCODE -ne 0)
+    {
+        Return 1
+    }
+}
+
+$kilobytes = (Get-Item -LiteralPath ".\$BinaryName.bin").Length / 1024
+Write-Output ("{0:#,##0.###} KiB" -f $kilobytes)
+
+If (-not $NoProgram)
+{
+    & 'C:\Program Files\OpenOCD\bin\openocd.exe' `
+        --command "source oocd-program.cfg"
+}
+
+If ($Debugger)
+{
+    & 'C:\Program Files\arm-gcc\bin\arm-none-eabi-gdb.exe' `
+        -ex "target extended-remote \\.\$DebugPort" `
+        -ex "monitor jtag_scan" `
+        -ex "attach 1" `
+        ".\target\thumbv7m-none-eabi\release\$BinaryName"
+}
