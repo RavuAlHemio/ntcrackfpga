5e08750dce7d599ac7ea0d754c8213abf4479fcc
Ondřej Hošek <ondra.hosek@gmail.com>
2022-11-07 21:27:37 +0100

    clkdiv: use on-board clock dividers

--- src/clockdiv.v
+++ src/clockdiv.v
@@ -1,35 +1,32 @@
-// ECP5 eval board: X2 is 200 MHz
-// nextpnr says:
-// Info: Max frequency for clock '$glbnet$clk$TRELLIS_IO_IN': 82.95 MHz (PASS at 12.00 MHz)
-// => divide by 3
-
 module clockdiv(
     input fullclock,
     output reg divclock);
 
 `ifdef SIMULATION
 
+// no clock division
 always #1 divclock = fullclock;
 
 `else
 
-// FIXME: use clockdiv on board
+// ECP5 eval board: X2 is 200 MHz
+// nextpnr says:
+// Info: Max frequency for clock '$glbnet$clk$TRELLIS_IO_IN': 82.95 MHz (PASS at 12.00 MHz)
+// => divide by (at least) 3
 
-reg [1:0] counter;
+wire passthrough;
 
-initial begin
-    counter <= 0;
-end
-
-always @ (posedge fullclock) begin
-    if (counter == 2) begin
-        counter <= 0;
-        divclock <= 1;
-    end else begin
-        counter <= counter + 1;
-        divclock <= 0;
-    end
-end
+// use clockdivs on board
+CLKDIVF #(.DIV("3.5")) internal_cdiv0(
+    .CLKI(fullclock),
+    .RST(1'b0),
+    .ALIGNWD(1'b0),
+    .CDIVX(passthrough));
+CLKDIVF #(.DIV("3.5")) internal_cdiv1(
+    .CLKI(passthrough),
+    .RST(1'b0),
+    .ALIGNWD(1'b0),
+    .CDIVX(divclock));
 
 `endif
 
