13c9f8c13f8b84bb1e2899dcbeedadba964e1f81
Ondřej Hošek <ondrej.hosek@tuwien.ac.at>
2022-11-08 14:18:29 +0100

    clockdiv: only divide once by 3.5

--- src/clockdiv.v
+++ src/clockdiv.v
@@ -11,25 +11,12 @@ always #1 divclock = fullclock;
 
 // ECP5 eval board: X2 is 200 MHz
 // nextpnr says:
-// Info: Max frequency for clock '$glbnet$clk$TRELLIS_IO_IN': 82.95 MHz (PASS at 12.00 MHz)
-// => divide by (at least) 3
-
-wire passthrough1;
-wire passthrough2;
+// Info: Max frequency for clock '$glbnet$div_clk': 69.95 MHz (PASS at 12.00 MHz)
+// => divide by 3.5 (57.14 MHz)
 
 // use clockdivs on board
 CLKDIVF #(.DIV("3.5")) internal_cdiv0(
     .CLKI(fullclock),
-    .RST(1'b0),
-    .ALIGNWD(1'b0),
-    .CDIVX(passthrough1));
-CLKDIVF #(.DIV("3.5")) internal_cdiv1(
-    .CLKI(passthrough1),
-    .RST(1'b0),
-    .ALIGNWD(1'b0),
-    .CDIVX(passthrough2));
-CLKDIVF #(.DIV("3.5")) internal_cdiv2(
-    .CLKI(passthrough2),
     .RST(1'b0),
     .ALIGNWD(1'b0),
     .CDIVX(divclock));
