a35f30f441680b97846837b28c262d57e34ca617
Ondřej Hošek <ondra.hosek@gmail.com>
2022-12-25 04:10:45 +0100

    try clock divisor 8 instead of 5

--- src/ntcrackfpga_hw.v
+++ src/ntcrackfpga_hw.v
@@ -11,8 +11,9 @@ wire clk;
 
 // nextpnr estimates our max clock at 70 MHz
 // base clock is ~310 MHz
-// => use a divisor of 5 (~62MHz)
-OSCG #(.DIV(5)) built_in_clock(.OSC(clk));
+// => we could use a divisor of 5 (~62MHz)
+// be conservative and use 8 (~38MHz)
+OSCG #(.DIV(8)) built_in_clock(.OSC(clk));
 
 ntcrackfpga cracker(
     clk,
